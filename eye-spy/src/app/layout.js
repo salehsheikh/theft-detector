0b677ff5a7afa511c069a60ed9049394e1af8d91
salehsheikh <salehsheikh786@gmail.com>
2025-07-06 15:49:58 +0500

    deploy

--- eye-spy/src/app/layout.js
+++ eye-spy/src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${karma.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${karma.variable} antialiased  bg-[var(--background)]`}
       >
 
         <AuthLayout>{children}</AuthLayout>
